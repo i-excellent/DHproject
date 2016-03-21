e3f456ed6d58509e33e05ec337882398c13ca130
iExellent <popov.dima1991@yandex.ua>
2016-03-21 10:51:35 +0200

    Update Subject.php

--- app/views/Subject.php
+++ app/views/Subject.php
@@ -1,1 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: losso
+ * Date: 22.03.2016
+ * Time: 11:49
+ */
+include ROOT.'/app/views/layouts/header.php';
+include ROOT.'/app/views/layouts/left_menu.php';
+?>
+    <div id="block_content">
+        <?php
 
+        ?>
+    </div>
+<?php
+include ROOT.'/app/views/layouts/footer.php';
+?>
